aad88ceb4afacfa62e120b8ea51711a14433677b
unknown <Alexandre Tonon Rosa@WCTONOROSA.unl.edu>
2018-10-19 11:33:46 -0500

    change cutoff to 300

--- fish-analysis.R
+++ fish-analysis.R
@@ -12,7 +12,7 @@ source("R/themes.R")
 fish <- read_csv("C:/Users/Alexandre Tonon Rosa/Box Sync/My Documents/Classes/Data Management, Visualization/2018-10-12-RClass/2018-10-12-RClass/gittutorial2/data/Gaeta_etal_CLC_data.csv")
 
 fish %>%
-  mutate(length_cat = case_when(length >= 200 ~ "big",
-                                length < 200 ~ "small")) -> fish
+  mutate(length_cat = case_when(length >= 300 ~ "big",
+                                length < 300 ~ "small")) -> fish
 head(fish)
                         
