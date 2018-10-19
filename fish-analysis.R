88c1e4c24b948b9f36b16c993dcfe0e5314469ad
unknown <Alexandre Tonon Rosa@WCTONOROSA.unl.edu>
2018-10-19 11:31:57 -0500

    fish data loaded

---
+++ fish-analysis.R
@@ -0,0 +1,18 @@
+## Challenge
+# library tidyverse
+# load data from data folder
+# create a new column in data called legth_cat length > 200 its big toerhwise small
+# commint w/ message
+# push
+
+.libPaths("C:/Users/Alexandre Tonon Rosa/Box Sync/My Documents/Classes/Data Management, Visualization/Rlibrary")
+library(tidyverse)
+source("R/themes.R")
+
+fish <- read_csv("C:/Users/Alexandre Tonon Rosa/Box Sync/My Documents/Classes/Data Management, Visualization/2018-10-12-RClass/2018-10-12-RClass/gittutorial2/data/Gaeta_etal_CLC_data.csv")
+
+fish %>%
+  mutate(length_cat = case_when(length >= 200 ~ "big",
+                                length < 200 ~ "small")) -> fish
+head(fish)
+                        
